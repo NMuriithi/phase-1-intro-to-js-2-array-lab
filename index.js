9890cffe1b85f80c4c8976cd346a220e88b36540
Nicholus <nicholus.muriithi@student.moringaschool.com>
2023-10-20 03:46:40 -0700

    Succesful Assignment Completion

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution here!
+ const cats = ["Milo", "Otis", "Garfield"]
+cats.push('Ralph');
+cats.unshift("Bob");
+cats.pop()
+cats.shift()
+function destructivelyAppendCat (name) {
+    cats.push("Ralph");
+    return cats;
+  }
+  console.log(cats)
+
+  function destructivelyPrependCat(name){
+   cats.unshift("Bob");
+  }
+function destructivelyRemoveLastCat(){
+   cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+   cats.shift()
+}
+function appendCat(){
+    return [...cats , "Broom"];
+}
+function prependCat(){
+    return ["Arnold" , ...cats]
+}
+function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+  function removeFirstCat() {
+ return cats.slice(1);
+}
